01c9e9c51de66eb0ee983a78c43634ee7654e6a8
cxduvall <57238295+cxduvall@users.noreply.github.com>
2022-01-26 22:14:09 -0600

    done.

--- lab1/sql/03.sql
+++ lab1/sql/03.sql
@@ -1,9 +1,15 @@
 -- What badge numbers (or ``stars")  have been assigned to more than one officer over the years?
-SELECT g.star
-    FROM (SELECT COUNT(*) as count, j.star
-            FROM (data_officer o JOIN data_officerbadgenumber b ON o.id = b.officer_id) AS j
-            GROUP BY j.star) AS g
-    WHERE g.count > 1
+
+--SELECT g.star
+--    FROM (SELECT COUNT(*) as count, j.star
+--            FROM (data_officer o JOIN data_officerbadgenumber b ON o.id = b.officer_id) AS j
+--            GROUP BY j.star) AS g
+--    WHERE g.count > 1
+--    ORDER BY g.star;
+
+SELECT DISTINCT b2.star
+            FROM (data_officerbadgenumber b1 JOIN data_officerbadgenumber b2
+                ON (b1.officer_id < b2.officer_id AND b1.star = b2.star))
 
 -- don't worry about order, and also not about duplicates??
 
